86994d7c7541cfafc8a3cac4eaad60a8ad24f2e3
Sergio Mover <sergio.mover@gmail.com>
2016-11-03 00:18:11 -0600

    Add execution of the community detection algorithm

--- scripts/full_extraction.bash
+++ scripts/full_extraction.bash
@@ -28,16 +28,22 @@ check_exists "${config_script}" "config.bash does not exists"
 source ${config_script}
 
 FIXR_GRAPH_EXTRACTOR_JAR="$(readlink -f ${FIXR_GRAPH_EXTRACTOR}/target/scala-2.11/fixrgraphextractor_2.11-0.1-SNAPSHOT-one-jar.jar)"
+FIXR_COMMUNITY_DETECTION_JAR="$(readlink -f ${FIXR_COMMUNITY_DETECTION}/target/scala-2.11/FixrCommunityDetection-assembly-0.1-SNAPSHOT.jar)"
 
 check_exists "${FIXR_GRAPH_EXTRACTOR}" "FixrGraphExtractor folder"
-check_exists "${FIXR_GRAPH_EXTRACTOR_JAR}" "FixrGraphExtractor jar does not exist"
+check_exists "${FIXR_GRAPH_EXTRACTOR_JAR}" "FixrGraphExtractor jar does not exist: did you run sbt oneJar there?"
 check_exists "${FIXR_GRAPH_INDEXER}" "FixrGraphIndexer folder"
 check_exists "${FIXR_GRAPH_ISO_BIN}" "FixrGraphIso binary"
+check_exists "${FIXR_COMMUNITY_DETECTION}" "FixrCommunityDetection path"
+check_exists "${FIXR_COMMUNITY_DETECTION_JAR}" "FixrCommunityDetection jar does not exist: did you run sbt assembly there?"
 check_exists "${OUT_DIR}" "Output directory"
 check_exists "${REPO_LIST}" "List of repositories"
+check_exists "${SPARK_SUBMIT_PATH}" "Spark submit executable"
 
 FIXR_GRAPH_PYTHON="$(readlink -f "${script_dir}/../python/fixrgraph/")"
 check_exists "${FIXR_GRAPH_PYTHON}" "Python package"
+
+
 
 OUT_LOG=${OUT_DIR}/out_log.txt
 
@@ -78,4 +84,14 @@ check_res "$?" "Processing logs"
 echo "Generating html pages..."
 python ${FIXR_GRAPH_PYTHON}/provenance/gen_html.py -d ${OUT_DIR}/graphs_db.db -o ${OUT_DIR}/index -g ${OUT_DIR}/graphs -p ${OUT_DIR}/provenance -i ${OUT_DIR}/iso &>> ${OUT_LOG}
 check_res "$?" "Generating html pages"
+
+
+echo "Running community detection..."
+# Generate the list of isomorphisms
+echo "Extracting isomorphism..."
+python ${FIXR_GRAPH_PYTHON}/db/scripts/filter_isos.py -d ${OUT_DIR}/graphs_db.db -o ${OUT_DIR}/iso_list.txt -w 10
+# Run the community detection
+echo "Computing communities..."
+echo "${SPARK_SUBMIT_PATH} --class edu.colorado.plv.fixr.community.Main ${FIXR_COMMUNITY_DETECTION_JAR} -w iso -i ${OUT_DIR}/iso -f ${OUT_DIR}/iso_list.txt -o ${OUT_DIR}/communities"
+${SPARK_SUBMIT_PATH} --class edu.colorado.plv.fixr.community.Main ${FIXR_COMMUNITY_DETECTION_JAR} -w iso -i ${OUT_DIR}/iso -f ${OUT_DIR}/iso_list.txt -o ${OUT_DIR}/communities
 
