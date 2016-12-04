7f55ebe5ca12034c76a76ff5687c5943130e778c
Sergio Mover <sergio.mover@gmail.com>
2016-12-03 23:07:25 -0700

    minor

--- scripts/full_extraction.bash
+++ scripts/full_extraction.bash
@@ -62,37 +62,43 @@ OUT_LOG=${OUT_DIR}/out_log.txt
 echo "Extracting graphs from ${REPO_LIST}..."
 pushd .
 cd ${OUT_DIR}
+echo "bash ${FIXR_GRAPH_PYTHON}/extraction/run_script.bash ${OUT_DIR} ${REPO_LIST} ${FIXR_GRAPH_EXTRACTOR_JAR} &>> ${OUT_LOG}" &>> ${OUT_LOG}
 bash ${FIXR_GRAPH_PYTHON}/extraction/run_script.bash ${OUT_DIR} ${REPO_LIST} ${FIXR_GRAPH_EXTRACTOR_JAR} &>> ${OUT_LOG}
 res=$?
 popd
 check_res "${res}" "Extract graphs of ${REPO_LIST}"
 
 echo "Filling graph dbs..."
-echo "python ${FIXR_GRAPH_PYTHON}/db/scripts/process_graphs.py -g ${OUT_DIR}/graphs -d ${OUT_DIR}/graphs_db.db &>> ${OUT_LOG}"
-python ${FIXR_GRAPH_PYTHON}/db/scripts/process_graphs.py -g ${OUT_DIR}/graphs -d ${OUT_DIR}/graphs_db.db &>> ${OUT_LOG}
+echo "python ${FIXR_GRAPH_PYTHON}/db/scripts/process_graphs.py -g ${OUT_DIR}/graphs -d ${OUT_DIR}/graphs_db.db &>> ${OUT_LOG}"   &>> ${OUT_LOG}
+python ${FIXR_GRAPH_PYTHON}/db/scripts/process_graphs.py -g ${OUT_DIR}/graphs -d ${OUT_DIR}/graphs_db.db &>> ${OUT_LOG} 
 check_res "$?" "Fill graph db"
 
 # Generate the index
 echo "Generating the index..."
+echo "java -jar ${FIXR_GRAPH_INDEXER}/target/scala-2.11/fixrgraphindexer_2.11-0.1-SNAPSHOT-one-jar.jar -d ${OUT_DIR}/graphs -o ${OUT_DIR}/iso_index.json -m ${MIN_METHODS} -n ${MIN_NODES} -e ${MIN_EDGES} -i ${MIN_COMMON_METHODS} &>> ${OUT_LOG}"  &>> ${OUT_LOG}
 java -jar ${FIXR_GRAPH_INDEXER}/target/scala-2.11/fixrgraphindexer_2.11-0.1-SNAPSHOT-one-jar.jar -d ${OUT_DIR}/graphs -o ${OUT_DIR}/iso_index.json -m ${MIN_METHODS} -n ${MIN_NODES} -e ${MIN_EDGES} -i ${MIN_COMMON_METHODS} &>> ${OUT_LOG}
 check_res "$?" "Index generation"
 
 # Compute the isomorphism
 echo "Schedule the isomorphism computation..."
+echo "python ${FIXR_GRAPH_PYTHON}/scheduler/create_jobs.py  -i ${OUT_DIR}/iso_index.json  -g ${OUT_DIR}/graphs -j ${OUT_DIR}/out_jobs -o ${OUT_DIR}/iso -s ${JOB_SIZE} -b ${FIXR_GRAPH_ISO_BIN} -p ${FIXR_GRAPH_PYTHON}/scheduler/run_iso.py -l /tmp -t ${TIMEOUT} &>> ${OUT_LOG}" &>> ${OUT_LOG}
 python ${FIXR_GRAPH_PYTHON}/scheduler/create_jobs.py  -i ${OUT_DIR}/iso_index.json  -g ${OUT_DIR}/graphs -j ${OUT_DIR}/out_jobs -o ${OUT_DIR}/iso -s ${JOB_SIZE} -b ${FIXR_GRAPH_ISO_BIN} -p ${FIXR_GRAPH_PYTHON}/scheduler/run_iso.py -l /tmp -t ${TIMEOUT} &>> ${OUT_LOG}
 check_res "$?" "Scheduling isomorphisms"
 
 pushd .
 cd ${OUT_DIR}/out_jobs
 echo "Computing isomorphisms..."
+echo "make -j -f scheduler_iso_index.make &>> ${OUT_LOG}" &>> ${OUT_LOG}
 make -j -f scheduler_iso_index.make &>> ${OUT_LOG}
 popd
 
 echo "Processing logs..."
+echo "python ${FIXR_GRAPH_PYTHON}/db/scripts/process_logs.py  -s ${OUT_DIR}/out_jobs/scheduler_iso_index.make -j ${OUT_DIR}/out_jobs -n ${OUT_DIR}/out_jobs -g ${OUT_DIR}/graphs -g ${OUT_DIR}/graphs -o ${OUT_DIR}/graphs_db.db &>> ${OUT_LOG}"  &>> ${OUT_LOG}
 python ${FIXR_GRAPH_PYTHON}/db/scripts/process_logs.py  -s ${OUT_DIR}/out_jobs/scheduler_iso_index.make -j ${OUT_DIR}/out_jobs -n ${OUT_DIR}/out_jobs -g ${OUT_DIR}/graphs -g ${OUT_DIR}/graphs -o ${OUT_DIR}/graphs_db.db &>> ${OUT_LOG}
 check_res "$?" "Processing logs"
 
 echo "Generating html pages..."
+echo "python ${FIXR_GRAPH_PYTHON}/provenance/gen_html.py -d ${OUT_DIR}/graphs_db.db -o ${OUT_DIR}/index -g ${OUT_DIR}/graphs -p ${OUT_DIR}/provenance -i ${OUT_DIR}/iso &>> ${OUT_LOG}" &>> ${OUT_LOG}
 python ${FIXR_GRAPH_PYTHON}/provenance/gen_html.py -d ${OUT_DIR}/graphs_db.db -o ${OUT_DIR}/index -g ${OUT_DIR}/graphs -p ${OUT_DIR}/provenance -i ${OUT_DIR}/iso &>> ${OUT_LOG}
 check_res "$?" "Generating html pages"
 
@@ -100,6 +106,7 @@ check_res "$?" "Generating html pages"
 echo "Running community detection..."
 # Generate the list of isomorphisms
 echo "Extracting isomorphism..."
+echo "python ${FIXR_GRAPH_PYTHON}/db/scripts/filter_isos.py -d ${OUT_DIR}/graphs_db.db -o ${OUT_DIR}/iso_list.txt -w 10 &>> ${OUT_LOG}" &>> ${OUT_LOG}
 python ${FIXR_GRAPH_PYTHON}/db/scripts/filter_isos.py -d ${OUT_DIR}/graphs_db.db -o ${OUT_DIR}/iso_list.txt -w 10 &>> ${OUT_LOG}
 check_res "$?" "Extracting isomorphism"
 
