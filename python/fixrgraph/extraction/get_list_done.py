7ce09a77630b5211b607e2cee0f2e76a57d3eb62
Sergio Mover <sergio.mover@gmail.com>
2017-02-04 18:14:41 -0700

    minor

--- python/fixrgraph/extraction/get_list_done.py
+++ python/fixrgraph/extraction/get_list_done.py
@@ -12,6 +12,7 @@ def main():
     p = optparse.OptionParser()
     p.add_option('-i', '--input', help="Input file")
     p.add_option('-o', '--output', help="Output file")
+    p.add_option('-s', '--status', help="Status to search for (0,1,2,3)")
 
     def usage(msg=""):
         if msg:
@@ -22,31 +23,37 @@ def main():
     opts, args = p.parse_args()
     if (not opts.input): usage("No input")
     if (not opts.output): usage("No output")
+    if (not opts.status): usage("No status")
     if (not os.path.isfile(opts.input)): usage("Input file does not existsnot")
 
 
+    repo_list = []
     with open(opts.input, "r") as f:
         data = json.load(f)
         assert "repo_status" in data
         for json_repo in data["repo_status"]:
             assert "status" in json_repo
-            if json_repo["status"] == 3:
+            if str(json_repo["status"]) == opts.status:
                 assert "repo" in json_repo
 
                 all_string = json_repo["repo"]
                 splitted = all_string.split("|")
                 assert (3 == len(splitted))
 
-                username = splitted[0]
-                reponame = splitted[1]
-                commit = splitted[2]
+                repo_list.append(all_string)
 
-                reposuffix = "%s/%s/%s" % (username, reponame, commit)
-                src = "/media/data/2016_10_28_extraction/%s" % reposuffix
-                dst = "/media/data/groum_sources/%s" % reposuffix
+        f.close()
 
-                print ("mkdir %s" % dst)
-                print ('rsync -zarv --include="*/" --include="*.java" --exclude="*" %s %s' % (src, dst))
+    with open(opts.output, "w") as fout:
+        repo_status = [ {"repo" : x, "status" : opts.status} for x in repo_list]
+        repo_list = [ {"repo" : x} for x in repo_list]
+        json_elem = {"repo_list" : repo_list, "repo_status" : repo_status}
+
+        print "Found %d repos in status %s" % (len(repo_status), opts.status)
+        json.dump(json_elem, fout)
+
+        fout.close()
+
 
 if __name__ == '__main__':
     main()
