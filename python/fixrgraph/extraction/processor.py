c4ec3494cd2b385ef963f692f40a005178f480da
smover <sergio.mover@gmail.com>
2017-10-22 15:09:11 -0600

    Start the implementation of a principled api for the extraction

--- python/fixrgraph/extraction/processor.py
+++ python/fixrgraph/extraction/processor.py
@@ -1,5 +1,7 @@
 # APIs used to perform the extraction of the ACDFGs (or groums)
 #
+# The class AppBuilderAPI allows to lookup the built artifacts for a repository
+#
 # The class RepoProcessor provides the functionalities to:
 #   1. Process a github repository and generate the ACDFGs.
 #     - the input is a github repository, an API to the Fixr builder data, and the output path to the results.
@@ -30,7 +32,6 @@ from xml.dom.minidom import parse, parseString
 # RepoVersion = collections.namedtuple('RepoVersion', 'user_name repo_name hash_str')
 
 class GitUtils:
-
     @staticmethod
     def get_key(user_name, repo_name, hash_str):
         """ Generate a key for a repo """
@@ -114,6 +115,145 @@ class AppBuilderAPI:
             return None
 
 
-#class RepoProcessor:
-#            logging.debug("Found repo %s in BuilderFarm " % (key))
+class RepoProcessor:
+    
+    @staticmethod
+    def extract_graph_from_repo_version():
+        
+
+    @staticmethod
+    def extract_static(repo,
+                       log,
+                       in_dir,
+                       android_home,
+                       graph_dir,
+                       prov_dir,
+                       classpath,
+                       extractor_jar,
+                       build_info,
+                       buildable_repos_path):
+        """ Extract all the graphs from the APK """
+
+        logging.info("Extracting graphs for repo: " + str(repo))
+
+
+        # get jar file from the android sdk
+        android_jar_path = RepoProcessor.get_android_jar(android_home, version_number)
+        if None == android_jar_path:
+            msg = "Cannot find the jar for %s/%s" % (repo[0], repo[1])
+            logging.debug(msg)
+            RepoProcessor.write_log(log, repo, msg)
+            return None
+
+        if 0 == len(process_dirs):
+            # Error, classes.jar not found
+            logging.debug("Cannot find classes.jar for %s/%s" % (repo[0], repo[1]))
+            RepoProcessor.write_log(log, repo, "Cannot find classes.jar for %s/%s" % (repo[0], repo[1]))
+            return None
+
+        # Small try on support libraries
+        logging.debug("Android jar path: %s for %s/%s" % (android_jar_path, repo[0], repo[1]))
+
+        try:
+            repo_graph_dir = RepoProcessor.get_repo_path(graph_dir, repo)
+            if not os.path.isdir(repo_graph_dir): os.makedirs(repo_graph_dir)
+
+            repo_prov_dir = RepoProcessor.get_repo_path(prov_dir, repo)
+            if not os.path.isdir(repo_prov_dir): os.makedirs(repo_prov_dir)
+
+            additional_cp = android_jar_path
+
+            def get_class_path(full_classes_path):
+                class_path = []
+                for p in full_classes_path.split("/"):
+                    class_path.append(p)
+                    # print p
+                    if p == "classes":
+                        break
+                return "/".join(class_path)
+            process_dirs_paths = set()
+            for pd in process_dirs:
+                process_dirs_paths.add(get_class_path(pd))
+            process_dirs = process_dirs_paths
+            for pd in process_dirs:
+                additional_cp = additional_cp + ":" + pd
+
+            if classpath != None:
+                classpath = classpath + ":" + additional_cp
+            else:
+                classpath = additional_cp
+
+            repo_url = RepoProcessor.get_repo_url(repo[0], repo[1])
+            # args = ["runlim",
+            #         "--time-limit=1200",
+            #         "java",
+            #         "-Xms%s" % MIN_HEAP_SIZE,
+            #         "-Xmx%s" % MAX_HEAP_SIZE,
+            #         "-jar", extractor_jar,
+            #         "-s", "false", # we read bytecode
+            #         "-l", classpath,
+            #         "-o", repo_graph_dir,
+            #         "-d", repo_prov_dir,
+            #         "-j", "true", # enable jphantom
+            #         "-z", repo_folder,
+            #         "-t", TIMEOUT,
+            #         "-n", repo[0],
+            #         "-r", repo[1],
+            #         "-u", repo_url]
+
+            args = ["java",
+                    "-Xms%s" % MIN_HEAP_SIZE,
+                    "-Xmx%s" % MAX_HEAP_SIZE,
+                    "-jar", extractor_jar,
+                    "-s", "false", # we read bytecode
+                    "-l", classpath,
+                    "-o", repo_graph_dir,
+                    "-d", repo_prov_dir,
+                    "-j", "true", # enable jphantom
+                    "-z", repo_folder,
+                    "-t", TIMEOUT,
+                    "-n", repo[0],
+                    "-r", repo[1],
+                    "-u", repo_url]
+
+
+            args.append("-p")
+
+            # remove google support libraries from the thing to
+            # process
+            new_process_dirs = []
+            ignore = ["/com.google.android.", "/com.android.",
+                      "/android."]
+            for p in process_dirs:
+                for i in ignore:
+                   if i in p:
+                       continue
+                new_process_dirs.append(p)
+            args.append(":".join(new_process_dirs))
+
+            if len(repo) > 2:
+                args.append("-h")
+                args.append(repo[2])
+
+            is_ok = RepoProcessor._call_sub(log, repo, args)
+            if not is_ok:
+                msg = "call_sub ended in error for %s/%s" % (repo[0], repo[1])
+                logging.debug(msg)
+                RepoProcessor.write_log(log, repo, msg)
+                return None
+
+        except Exception as e:
+            traceback.print_exc()
+
+            # DEBUG
+            logging.debug("Cannot extract the graphs from %s/%s/%s" % (repo[0], repo[1], repo[2]))
+            RepoProcessor.write_log(log, repo, e.message)
+            return None
+
+        logging.debug("Extraction of graph ended for %s/%s/%s" % (repo[0], repo[1],repo[2]))
+        return repo
+
+
+
+    logging.debug("Found repo %s in BuilderFarm " % (key))
 
