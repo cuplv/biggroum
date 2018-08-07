c8f16056d54623ea22e677003db461f3a7cf479b
smover <sergio.mover@gmail.com>
2018-08-07 17:05:35 -0600

    Update docker compose for two six labs

--- docker_compose/get_docker_compose.py
+++ docker_compose/get_docker_compose.py
@@ -14,27 +14,43 @@ def _substitute(template, substitution):
 
 NEXUS_IP = "192.168.0.2"
 NEXUS_PORT = "5005"
-VERSION = "0.2"
 
 LOCAL_IMAGES = {"SOLR_IMAGE" : "biggroum_solr",
                 "SEARCH_IMAGE" : "biggroum_search",
                 "SRC_IMAGE" : "srcfinder",
                 "FRONTEND_IMAGE" : "biggroum_frontend",
                 "SOLR_DATA_PREFIX" : "../FixrGraphPatternSearch",
-                "SEARCH_DATA_PREFIX" : "../FixrGraphPatternSearch"}
+                "SEARCH_DATA_PREFIX" : "../FixrGraphPatternSearch",
+                "RESOURCES_BIGGROUM_SOLR" : "",
+                "RESOURCES_BIGGROUM_SEARCH" : "",
+                "RESOURCES_SRCFINDER" : "",
+                "RESOURCES_BIGGROUM_FRONTEND" : ""}
 
 REP = (NEXUS_IP, NEXUS_PORT)
 REMOTE_IMAGES = {"SOLR_IMAGE" : "%s:%s/biggroum_solr:${VERSION}" % REP,
                  "SEARCH_IMAGE" : "%s:%s/biggroum_search:${VERSION}" % REP,
                  "SRC_IMAGE" : "%s:%s/srcfinder:${VERSION}" % REP,
                  "FRONTEND_IMAGE" : "%s:%s/biggroum_frontend:${VERSION}" % REP,
-                 "SOLR_DATA_PREFIX" : "..",
-                 "SEARCH_DATA_PREFIX" : ".."}
+                 "SOLR_DATA_PREFIX" : "/srv/col_solr",
+                 "SEARCH_DATA_PREFIX" : "/srv/col-search",
+                 "RESOURCES_BIGGROUM_SOLR" : """cpu_count: 2
+    mem_limit: 2048000000
+    network_mode: "bridge" """,
+                 "RESOURCES_BIGGROUM_SEARCH" : """cpu_count: 2
+    mem_limit: 4096000000
+    network_mode: "bridge" """,
+                 "RESOURCES_SRCFINDER" : """cpu_count: 2
+    mem_limit: 2048000000
+    network_mode: "bridge" """,
+                 "RESOURCES_BIGGROUM_FRONTEND" : """cpu_count: 2
+    mem_limit: 2048000000
+    network_mode: "bridge" """}
 
-DOCKER_FILE = """version: '3'
+DOCKER_FILE = """version: '2.2'
 services:
   biggroum_solr:
     image: ${SOLR_IMAGE}
+    ${RESOURCES_BIGGROUM_SOLR}
     ports:
     - "30071:8983"
     hostname : biggroum_solr
@@ -43,6 +59,7 @@ services:
 
   biggroum_search:
     image: ${SEARCH_IMAGE}
+    ${RESOURCES_BIGGROUM_SEARCH}
     ports:
     - "30072:8081"
     links:
@@ -53,17 +70,18 @@ services:
 
   srcfinder:
     image: ${SRC_IMAGE}
+    ${RESOURCES_SRCFINDER}
     hostname: srcfinder
 
   biggroum_frontend:
     image: ${FRONTEND_IMAGE}
+    ${RESOURCES_BIGGROUM_FRONTEND}
     ports:
     - "30073:5000"
     links:
     - biggroum_search
     - srcfinder
     hostname: biggroum_frontend
-
 """
 
 def main(input_args=None):
