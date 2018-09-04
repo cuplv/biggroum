9e49c3fbfff42ed95396cccb028d6cfd89bb59af
smover <sergio.mover@gmail.com>
2018-09-04 08:59:38 -0600

    Update tag and commit

--- docker_compose/tag_and_commit.bash
+++ docker_compose/tag_and_commit.bash
@@ -1,12 +1,9 @@
 #!/bin/bash
 
-version=0.2
+version=2.0
 
 echo "Check that you are connected to the VPN..."
 docker login http://100.120.0.2:5005
-
-docker tag biggroum_solr 100.120.0.2:5005/biggroum_solr:${version}
-docker push 100.120.0.2:5005/biggroum_solr:${version}
 
 docker tag biggroum_search 100.120.0.2:5005/biggroum_search:${version}
 docker push 100.120.0.2:5005/biggroum_search:${version}
