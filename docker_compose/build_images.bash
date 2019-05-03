2521d55a27c8e9f33c897da44a13d37fa6595afa
smover <sergio.mover@gmail.com>
2019-05-03 09:43:12 +0200

    Removed unuseful pop commands

--- docker_compose/build_images.bash
+++ docker_compose/build_images.bash
@@ -1,21 +1,19 @@
 #!/bin/bash
 
 # Search
-popd
 pushd .
 cd ../FixrGraphPatternSearch/docker_search
 # bash download_data.bash
 docker build -t biggroum_search .
+popd
 
 # src server
-popd
 pushd .
 cd ../fixr_source_code_service/docker/
 docker build -t srcfinder .
 popd
 
 # web interface
-popd
 pushd .
 cd ../fixr_groum_search_frontend/docker/
 docker build -t biggroum_frontend .
