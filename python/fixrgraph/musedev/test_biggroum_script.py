5fe421fe7348b17d15f9db84bf0f35a87ab39f9c
smover <sergio.mover@gmail.com>
2019-11-12 10:23:32 +0100

    Minor

--- python/fixrgraph/musedev/test_biggroum_script.py
+++ python/fixrgraph/musedev/test_biggroum_script.py
@@ -17,19 +17,6 @@ except ImportError:
 from biggroumscript import main
 from api import biggroum_api_map
 
-# Hold on on this
-# @contextmanager
-# from contextlib import contextmanager
-# def captured_output():
-#     new_out, new_err = StringIO(), StringIO()
-#     old_out, old_err = sys.stdout, sys.stderr
-#     try:
-#         sys.stdout, sys.stderr = new_out, new_err
-#         yield sys.stdout, sys.stderr
-#     finally:
-#         sys.stdout, sys.stderr = old_out, old_err
-
-
 class TestScript(unittest.TestCase):
     FILEPATH = os.path.join(os.path.dirname(__file__), "test_data")
     JAVAFILE = "AwesomeApp/app/src/main/java/fixr/plv/colorado/edu/awesomeapp/MainActivity.java"
