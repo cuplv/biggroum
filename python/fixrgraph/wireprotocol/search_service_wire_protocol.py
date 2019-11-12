8b99f9a5e534ebcb1331fe741e3345406d35e46c
Shawn Meier <shawn.meier@colorado.edu>
2019-11-12 14:16:12 -0700

    skeleton for search service wire protocol python module

---
+++ python/fixrgraph/wireprotocol/search_service_wire_protocol.py
@@ -0,0 +1,15 @@
+
+
+def compress(file_list):
+    """
+    :param file_list: a list of strings representing fully qualified paths to files
+    :return: path to compressed zip
+    """
+    pass
+
+def decompress(zip_file):
+    """
+    :param zip_file: path to zip
+    :return:
+    """
+    pass
