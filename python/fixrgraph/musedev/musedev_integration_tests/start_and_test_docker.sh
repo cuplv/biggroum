4d8f134d17381b1942f9b05e961929c10be9eaa6
Ubuntu <ubuntu@ip-172-31-1-4.us-east-2.compute.internal>
2019-11-20 22:40:39 +0000

    update start and test script to be compatible on ubuntu machine

--- python/fixrgraph/musedev/musedev_integration_tests/start_and_test_docker.sh
+++ python/fixrgraph/musedev/musedev_integration_tests/start_and_test_docker.sh
@@ -6,10 +6,18 @@ cd "$(dirname "${BASH_SOURCE[0]}")/../../../../docker_compose/"
 # Get docker compose for integration tests
 python get_docker_compose.py --remote -v latest -m -d
 
-docker-compose up --force-recreate --detach
+docker-compose up --force-recreate &
 
-bash ../python/fixrgraph/musedev/musedev_integration_tests/test_docker.sh docker_compose_fixrgraph_1
+while [[ ! $(docker ps |grep dockercompose_fixrgraph) ]]
+do
+	sleep 2
+	echo "Waiting for containers to start..."
+done
 
-docker-compose down
+ANALYST_NAME=$(docker ps --format "{{.Names}}" |grep fixrgraph)
+
+bash ../python/fixrgraph/musedev/musedev_integration_tests/test_docker.sh ${ANALYST_NAME}
+
+#docker-compose down
 
 popd
