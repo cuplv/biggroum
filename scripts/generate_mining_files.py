f73bddd568d2752564b122c658c7ae5079608fa7
Shawn Meier <shawn.meier@colorado.edu>
2019-12-12 14:51:31 -0700

    fix config yaml generation

--- scripts/generate_mining_files.py
+++ scripts/generate_mining_files.py
@@ -4,6 +4,7 @@ import json
 import shutil
 import tarfile
 
+CONFIG_DIR = "mining_configuration"
 
 class IDGen:
     current_id = 1
@@ -21,30 +22,37 @@ def find_with_extension(path, extension):
                 paths.append(os.path.join(root,f))
     return paths
 
-def generate_repo_list(path,out_path = None, extract_sources=None):
+def generate_repo_list(path,out_path = None, extract_sources=None, limit_number = None):
     if out_path is None:
         out_path = "./"
     elif not os.path.isdir(out_path):
         raise Exception("Output directory %s does not exist" % out_path)
     if (not os.path.isdir(path)):
         print("Error: directory \"%s\" does not exist" % path)
-    if (not os.path.exists(os.path.join(out_path,"mining_configuration"))):
-        os.mkdir("mining_configuration")
+    if (not os.path.exists(os.path.join(out_path,CONFIG_DIR))):
+        os.mkdir(CONFIG_DIR)
         
     repo_list = []
     buildable_list = []
 
-    for root, dirs, files in os.walk(path):
+    processed_count = 0
+    for root, dirs, files in os.walk(path): #TODO: randomize order
         if "completed.txt" in files:
+            if (limit_number is not None) and (processed_count >= limit_number):
+                break
+            processed_count += 1
             print("Processing directory %s" % root)
             data = generate_repo_list_item(root)
             repo_list.append(data)
             buildable_list.append(generate_build_list_item(root, extract_sources))
-    with open(os.path.join(out_path,"mining_configuration/repo_list.json"), "w") as f:
+
+    repo_list_path = os.path.join(out_path, "mining_configuration/repo_list.json")
+    with open(repo_list_path, "w") as f:
         json.dump(repo_list, f, indent=4, sort_keys=True)
-    with open(os.path.join(out_path,"mining_configuration","buildable.json"),'w') as f:
+    buildable_list_path = os.path.join(out_path, "mining_configuration", "buildable.json")
+    with open(buildable_list_path, 'w') as f:
         json.dump({"results": buildable_list}, f, indent=4, sort_keys=True)
-    return repo_list
+    return (repo_list_path, buildable_list_path)
 
 
 def generate_repo_list_item(fdroid_project_base_dir):
@@ -98,15 +106,20 @@ def generate_build_list_item(fdroid_project_base_dir, extract_sources=None):
 
     return data
 
-
-
+def getConfig(repo_list_file, buildable_file, build_data_dir,output_path):
+    with open("config_template.txt",'r') as f:
+        template = f.read()
+        return template.format(repo_list=repo_list_file,
+                               buildable=buildable_file,build_data_dir=build_data_dir,output_path=output_path)
 
 def main():
     p = optparse.OptionParser()
     p.add_option('-p', '--path', help="Path to directory containing data")
-    p.add_option('-o', '--output_path', help="directory to output config files")
+    p.add_option('-o', '--output_path', help="Directory to output config files")
     p.add_option('-e', '--extract_src_dir',
                  help="Optional: directory to extract sources zip, do not put inside fdroid folder")
+    p.add_option('-n', '--limit_number', type=int)
+    p.add_option('-b', '--build_data_dir', help="Base directory of build data")
     opts, args = p.parse_args()
 
     def usage(msg=""):
@@ -118,7 +131,14 @@ def main():
     if (not opts.path):
         usage("No path specified")
 
-    generate_repo_list(opts.path, opts.output_path, opts.extract_src_dir)
+    (repo_list_path, buildable_list_path) = \
+        generate_repo_list(opts.path, opts.output_path, opts.extract_src_dir,opts.limit_number)
+    config = getConfig(repo_list_path, buildable_list_path, opts.build_data_dir, opts.output_path)
+
+    with open(os.path.join(opts.output_path, CONFIG_DIR, "config.yaml"), 'w') as config_file:
+        config_file.write(config)
+
+
 
 if __name__ == "__main__":
     main()
